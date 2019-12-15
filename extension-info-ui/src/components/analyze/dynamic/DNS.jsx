19e8ffd338b3cbf208d49299968363e0e866ae4d
Son Nguyen Hong <hongson.uit@gmail.com>
2019-12-15 14:36:50 +0700

    Update DNS.jsx

--- extension-info-ui/src/components/analyze/dynamic/DNS.jsx
+++ extension-info-ui/src/components/analyze/dynamic/DNS.jsx
@@ -39,12 +39,12 @@ class API extends Component {
           {
             Header: 'CName',
             style: {'whiteSpace': 'unset'},
-            accessor: "response.resolver.CNAME"
+            accessor: "response.cname_domain"
           },
           {
             Header: 'IP Server',
             style: {'whiteSpace': 'unset'},
-            accessor: "response.resolver.IP"
+            accessor: "response.ip_server"
           },
           
         ]
@@ -59,7 +59,6 @@ class API extends Component {
     const apiData = Object.keys(rawApiData).map(i => {
       return Object.assign({}, {api: i}, rawApiData[i])
     })
-    console.log(apiData)
     const newApiData = apiData.map(item => {
       if(typeof item.response === 'undefined')
       {
@@ -75,6 +74,23 @@ class API extends Component {
           }
         }
       }
+      let result = item.response.resolver.IP
+      if(item.response.resolver.IP.length > 1)
+      {
+        result = item.response.resolver.IP.reduce((temp,item)=> {
+          return temp +item+', '
+        },"")
+      }
+
+      let result_cname = item.response.resolver.CNAME
+      if(item.response.resolver.CNAME.length > 1)
+      {
+        result_cname = item.response.resolver.CNAME.reduce((temp,item)=> {
+          return temp +item.substr(0,item.length -1 )+', '
+        },"")
+      }
+
+
       return {
         ...item,
         request: {
@@ -83,11 +99,13 @@ class API extends Component {
         },
         response: {
           ...item.response,
-             source: `${item.response.source_ip}:${item.response.source_port}`
+            source: `${item.response.source_ip}:${item.response.source_port}`,
+            ip_server: result,
+            cname_domain : result_cname
         }
       }
     })
-
+    console.log(newApiData)
 
     
 
